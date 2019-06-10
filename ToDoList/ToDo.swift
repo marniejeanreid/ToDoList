6162d6b016083465caf0927aca894b24be5216e4
Marnie Reid <marniejeanreid@gmail.com>
2019-06-09 21:43:11 -0700

    Partial Iteration 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Marnie Reid on 6/9/19.
+//  Copyright © 2019 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name : String
+    var important : Bool = false
+    
+    init() {
+        name = ""
+        important = false
+        //check that this is initialized correctly
+    }
+}
